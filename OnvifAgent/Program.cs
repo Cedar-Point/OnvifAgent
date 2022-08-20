a6ccff49e132d0cc8ab1b6c357a3f4f1f8ef6722
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2022-08-20 16:14:18 -0400

    Update Program.cs

--- OnvifAgent/Program.cs
+++ OnvifAgent/Program.cs
@@ -8,14 +8,14 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-WebClient wc = new();
-
 app.MapPost("/{IPAddress}", async (e) =>
 {
     try
     {
+        WebClient wc = new();
         wc.Credentials = new NetworkCredential(e.Request.Query["user"], e.Request.Query["pass"]);
         string result = await wc.UploadStringTaskAsync("http://" + e.Request.RouteValues["IPAddress"] + "/onvif/device_service", await new StreamReader(e.Request.Body).ReadToEndAsync());
+        wc.Dispose();
         await e.Response.WriteAsync(result);
     }
     catch (Exception ex)
