8392a56067e9152c62ab4ab9fe884a0a9165804e
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2022-09-08 11:32:09 -0400

    Update Program.cs

--- OnvifAgent/Program.cs
+++ OnvifAgent/Program.cs
@@ -12,9 +12,11 @@ app.MapPost("/{IPAddress}", async (e) =>
 {
     try
     {
+        string path = "/onvif/device_service";
+        if (e.Request.Query["path"] != null) path = e.Request.Query["path"];
         WebClient wc = new();
         wc.Credentials = new NetworkCredential(e.Request.Query["user"], e.Request.Query["pass"]);
-        string result = await wc.UploadStringTaskAsync("http://" + e.Request.RouteValues["IPAddress"] + "/onvif/device_service", await new StreamReader(e.Request.Body).ReadToEndAsync());
+        string result = await wc.UploadStringTaskAsync("http://" + e.Request.RouteValues["IPAddress"] + path, await new StreamReader(e.Request.Body).ReadToEndAsync());
         wc.Dispose();
         await e.Response.WriteAsync(result);
     }
